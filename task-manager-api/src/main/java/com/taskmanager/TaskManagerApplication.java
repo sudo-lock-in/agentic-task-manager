63d53397f11b71a8bfe6eabfdda64d8f95ebb705
sudo-lock-in <amythompson0325@gmail.com>
2026-05-16 12:44:39 -0400

    Fix formatting in TaskManagerApplication class

--- task-manager-api/src/main/java/com/taskmanager/TaskManagerApplication.java
+++ task-manager-api/src/main/java/com/taskmanager/TaskManagerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class TaskManagerApplication {
+public class TaskManagerApplication { 
 
     public static void main(String[] args) {
         SpringApplication.run(TaskManagerApplication.class, args);
